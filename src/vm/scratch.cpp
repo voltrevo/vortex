672f854f6552b70eb7697336825704c6c9973cdc
Andrew Morris <voltrevo@gmail.com>
2018-11-08 18:17:42 +1100

    Test loops with project euler problem 1

--- src/vm/scratch.cpp
+++ src/vm/scratch.cpp
@@ -45,6 +45,17 @@ int main() {
     RETURN, // 50
     */
 
+    /*
+    // sum := 0;
+    // i := 1;
+    // for {
+    //   sum += i;
+    //   if (i == 4) {
+    //     break;
+    //   }
+    //   i += 1;
+    // }
+    // return sum;
     INT32, 0, 0, 0, 0,
     SET_LOCAL, 0,
 
@@ -68,6 +79,56 @@ int main() {
       INT32, 1, 0, 0, 0,
       PLUS,
       SET_LOCAL, 1,
+    END,
+
+    GET_LOCAL, 0,
+    RETURN,
+    */
+
+    //sum := 0;
+    //for (i := 1; i < 1000; i++) {
+    //  if (i % 3 == 0 || i % 5 == 0) {
+    //    sum += i;
+    //  }
+    //}
+    //return sum;
+
+    INT32, 0, 0, 0, 0,
+    SET_LOCAL, 0,
+
+    INT32, 1, 0, 0, 0,
+    SET_LOCAL, 1,
+
+    LOOP,
+      GET_LOCAL, 1,
+      INT32, 3, 0, 0, 0,
+      MODULUS,
+      INT32, 0, 0, 0, 0,
+      EQUAL,
+      GET_LOCAL, 1,
+      INT32, 5, 0, 0, 0,
+      MODULUS,
+      INT32, 0, 0, 0, 0,
+      EQUAL,
+      OR,
+      IF,
+        GET_LOCAL, 0,
+        GET_LOCAL, 1,
+        PLUS,
+        SET_LOCAL, 0,
+      END,
+
+      GET_LOCAL, 1,
+      INT32, 1, 0, 0, 0,
+      PLUS,
+      SET_LOCAL, 1,
+
+      GET_LOCAL, 1,
+      INT32, 0xe8, 3, 0, 0,
+      EQUAL,
+      IF,
+        BREAK,
+      END,
     END,
 
     GET_LOCAL, 0,
