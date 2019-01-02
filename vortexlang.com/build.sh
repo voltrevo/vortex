9a29b6b7f269b935da200215cc9f1d0808ac93da
Andrew Morris <voltrevo@gmail.com>
2019-01-03 09:01:18 +1100

    Add viewport meta tags

--- vortexlang.com/build.sh
+++ vortexlang.com/build.sh
@@ -10,4 +10,4 @@ rm build/website/playground/*.ts
 mv build/website/playground/playground.bundle.js build/website/playground/index.js
 
 cd build/website
-npx live-server --no-browser
+npx live-server . --no-browser --host 0.0.0.0
