90452a116b2aac6020e564023d704d41846cc5da
Andrew Morris <voltrevo@gmail.com>
2019-02-14 11:44:05 +1100

    Add new vxc-lines scaffolding and bootparse to run bootstrapped parser on stdin

---
+++ src/vxc-lines.ts
@@ -0,0 +1,93 @@
+import * as path from 'path';
+
+import * as minimist from 'minimist';
+
+import Analyzer from './Analyzer';
+import Compiler from './Compiler';
+import FileReader from './FileReader';
+import getStdin from './getStdin';
+import Note from './Note';
+import Outcome from './Analyzer/Outcome';
+import PackageRoot from './PackageRoot';
+import pretty from './pretty';
+
+const args = minimist(process.argv.slice(2));
+
+if (args._.length !== 1) {
+  throw new Error('Usage: vxc-lines <file>.vx');
+}
+
+let resolvedFile = path.resolve(process.cwd(), args._[0]);
+
+const packageRoot = PackageRoot(path.dirname(resolvedFile));
+
+if (resolvedFile.slice(0, packageRoot.length + 1) !== packageRoot + path.sep) {
+  throw new Error(
+    `Should not be possible: ${resolvedFile} is outside package ${packageRoot}`
+  );
+}
+
+const entryFile = resolvedFile.replace(packageRoot, '@');
+
+const readFile = FileReader(packageRoot);
+
+let [notes, az] = Compiler.compile(
+  [entryFile],
+  readFile,
+  { stepLimit: 1000000 },
+);
+
+notes = Note.flatten(notes as Note[]);
+let hasErrors = false;
+
+for (const note of notes) {
+  if (note.level !== 'error') {
+    continue;
+  }
+
+  pretty.print(note, readFile(note.pos[0]));
+  hasErrors = true;
+}
+
+if (hasErrors) {
+  throw new Error('Aborting - failed to compile');
+}
+
+const entryMod = az.modules[entryFile];
+
+if (!entryMod || !entryMod.loaded) {
+  throw new Error('Should bail earlier');
+}
+
+if (entryMod.outcome === null) {
+  throw new Error('Should bail earlier');
+}
+
+const entryFunc = entryMod.outcome;
+
+if (entryFunc.t !== 'Func') {
+  throw new Error(
+    'Expected a Func to be returned from ' + entryFile +
+    ' but got ' + entryFunc.t
+  );
+}
+
+(async () => {
+  const stdinText = await getStdin();
+
+  const lines = Outcome.Array(stdinText.split('\n').map(Outcome.String));
+
+  debugger;
+
+  let output: Outcome;
+  [output, az] = Analyzer.analyze.functionCallValue(
+    {...az, steps: 0, stepLimit: null},
+    null,
+    entryFunc,
+    [lines],
+  );
+
+  console.log(Outcome.LongString(output));
+})().catch(e => {
+  setTimeout(() => { throw e; });
+});
