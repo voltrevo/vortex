4c51141a7f88371d4915e177cd703329dc393140
Andrew Morris <voltrevo@gmail.com>
2020-10-13 07:39:28 +1100

    Implement AddAssign

--- src/vm-rs/src/vortex/value.rs
+++ src/vm-rs/src/vortex/value.rs
@@ -21,10 +21,12 @@ impl fmt::Display for Value {
         match self {
             Value::Null => write!(f, "null"),
             Value::Bool(x) => write!(f, "{}", x),
+
             Value::U8(x) => write!(f, "{}u8", x),
             Value::U16(x) => write!(f, "{}u16", x),
             Value::U32(x) => write!(f, "{}u32", x),
             Value::U64(x) => write!(f, "{}u64", x),
+
             Value::I8(x) => write!(f, "{}i8", x),
             Value::I16(x) => write!(f, "{}i16", x),
             Value::I32(x) => write!(f, "{}i32", x),
@@ -33,22 +35,29 @@ impl fmt::Display for Value {
     }
 }
 
-impl ops::Add for Value {
-    type Output = Value;
-
-    fn add(self, other: Value) -> Value {
+impl ops::AddAssign for Value {
+    fn add_assign(&mut self, other: Value) {
         match (self, other) {
-            (Value::U8(left), Value::U8(right)) => Value::U8(left + right),
-            (Value::U16(left), Value::U16(right)) => Value::U16(left + right),
-            (Value::U32(left), Value::U32(right)) => Value::U32(left + right),
-            (Value::U64(left), Value::U64(right)) => Value::U64(left + right),
-            
-            (Value::I8(left), Value::I8(right)) => Value::I8(left + right),
-            (Value::I16(left), Value::I16(right)) => Value::I16(left + right),
-            (Value::I32(left), Value::I32(right)) => Value::I32(left + right),
-            (Value::I64(left), Value::I64(right)) => Value::I64(left + right),
+            (Value::U8(left), Value::U8(right)) => *left += right,
+            (Value::U16(left), Value::U16(right)) => *left += right,
+            (Value::U32(left), Value::U32(right)) => *left += right,
+            (Value::U64(left), Value::U64(right)) => *left += right,
+
+            (Value::I8(left), Value::I8(right)) => *left += right,
+            (Value::I16(left), Value::I16(right)) => *left += right,
+            (Value::I32(left), Value::I32(right)) => *left += right,
+            (Value::I64(left), Value::I64(right)) => *left += right,
 
             _ => panic!("Type error"),
         }
+    }
+}
+
+impl ops::Add for Value {
+    type Output = Value;
+
+    fn add(mut self, other: Value) -> Value {
+        self += other;
+        self
     }
 }
